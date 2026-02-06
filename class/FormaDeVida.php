99b15f9ff1754af969fe385515804928875e605f
Miguel <mchaparrosanchis@gmail.com>
2026-02-06 22:27:42 +0100

    Clase Forma Vida

---
+++ class/FormaDeVida.php
@@ -0,0 +1,38 @@
+<?php
+
+class FormaDeVida extends EntidadEstelar {
+    private $dieta;
+
+    public function __construct($id, $nombre, $planetaOrigen, $nivelEstabilidad, $dieta) {
+        parent::__construct($id, $nombre, $planetaOrigen, $nivelEstabilidad);
+        $this->dieta = $dieta;
+    }
+
+    public function getDieta() {
+        return $this->dieta;
+    }
+
+    public function setDieta($dieta) {
+        $this->dieta = $dieta;
+    }
+
+    public function reaccionar() {
+        return "La forma de vida '{$this->nombre}' emite señales bioquímicas desde {$this->planetaOrigen}.";
+    }
+
+    public function getTipo() {
+        return 'FormaDeVida';
+    }
+
+    public function getCampoEspecial() {
+        return $this->dieta;
+    }
+
+    public function setCampoEspecial($valor) {
+        $this->dieta = $valor;
+    }
+
+    public function getNombreCampoEspecial() {
+        return 'Dieta';
+    }
+}
